46d2a27609b7477ccae48bf8af52a666ef2a90e4
Banjeazy <45882335+Banjeazy@users.noreply.github.com>
2018-12-19 01:48:22 +0300

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 
 public class Simple {
+   
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        
         int top = scanner.nextInt();
 
         for (int i = 2; i < top; i++) {
@@ -31,6 +33,7 @@ public class Simple {
 
             if (i % n == 0 || i % (n + 2) == 0)
                 return false;
+            
             n = n + 6;
         }
         return true;
