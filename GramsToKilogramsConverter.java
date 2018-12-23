90652a1683c7440ae7d5ec1a586299940bc2621a
Banjeazy <nerlone@yandex.ru>
2018-12-23 23:56:05 +0300

    run,Barry,run

--- GramsToKilogramsConverter.java
+++ GramsToKilogramsConverter.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class GramsToKilogramsConverter {
-    public static void main(String[] args) {
+    public static void convertGramsToKg() {
         System.out.print("Введите граммы: ");
          Scanner scan = new Scanner(System.in);
          int grams = scan.nextInt();
