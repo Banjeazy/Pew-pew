55296a284c52d4d8eade91c2c5badaaf32954f72
Banjeazy <nerlone@yandex.ru>
2018-12-24 22:46:55 +0300

    gmkg

--- GramsToKilogramsConverter.java
+++ GramsToKilogramsConverter.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-
 public class GramsToKilogramsConverter {
-    public static void convertGramsToKg() {
-        System.out.print("Введите граммы: ");
-         Scanner scan = new Scanner(System.in);
-         int grams = scan.nextInt();
-         double kilograms = grams * 0.001;
-        System.out.println("Килограмм: " +  kilograms);
-      }
+
+    public static double convertGramsToKg(double grams) {
+        double kilograms = grams * 0.001;
+
+        return kilograms;
+    }
 }
