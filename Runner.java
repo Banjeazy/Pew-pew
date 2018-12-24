298c38f86883edc75c1fd63a4e2fbc14796365d5
IncssblCrdnl <32500184+BuhaiVolha@users.noreply.github.com>
2018-12-24 18:48:44 +0300

    Update Runner.java

--- Runner.java
+++ Runner.java
@@ -2,4 +2,9 @@ public class Runner {
     public static void main(String[] args){
         GramsToKilogramsConverter.convertGramsToKg();
     }
+    
+        public static void main(String[] args) {
+            double grams = 1500;
+            double kg = GramsToKilogramsConverter.convertGramsToKg(grams);
+    }
 }
