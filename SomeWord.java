95fb436c3497fb2b52893ced18817c4eb8782b33
Banjeazy <nerlone@yandex.ru>
2019-01-10 21:56:12 +0300

    word

---
+++ SomeWord.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class SomeWord {
+
+    public static void main(String[] args) {
+        String str = "some word";
+        String word = str.toUpperCase();
+        System.out.println(word);
+    }
+}
