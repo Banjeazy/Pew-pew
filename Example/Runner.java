9e5d4058364fbd98855dbe71cc6d5faab3c73596
Banjeazy <nerlone@yandex.ru>
2019-01-12 15:10:18 +0300

    someword

---
+++ Example/Runner.java
@@ -0,0 +1,8 @@
+package Example;
+
+public class Runner {
+
+    public static void main(String[] args){
+        String word = SomeWord.wordSome();
+    }
+}
---
+++ Example/SomeWord.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class SomeWord {
+
+    public static String wordSome() {
+        String str = "some word";
+        String strUpper = str.toUpperCase();
+        return str;
+    }
+}
