b18796e22170bec7136a1dcbcf2fc0f9386fc3ba
Banjeazy <nerlone@yandex.ru>
2019-01-18 01:15:29 +0300

    some word

---
+++ Example/StringUpperСase.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class StringUpperСase {
+
+    public static String convertStringUpperCase() {
+        String word = "some word";
+        String str = word.toUpperCase();
+        return str;
+    }
+}
