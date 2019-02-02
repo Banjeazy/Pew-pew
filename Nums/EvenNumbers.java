a4711ba450bb43110db51cebb6705655f1229f7f
Banjeazy <nerlone@yandex.ru>
2019-02-03 02:06:04 +0300

    nums

--- Nums/EvenNumbers.java
+++ Nums/EvenNumbers.java
@@ -2,11 +2,11 @@ package task.Nums;
 public class EvenNumbers {
 
     public static int[] arrayEvenNums() {
-        int[] nums = new int[]{1, 5, 9, 20, 89, 45, 22, 95, 57};
+        int nums[] = {1, 5, 9, 20, 89, 45, 22, 95, 57};
         {
-            for (int i = 0; i < nums.length; i++) ;
-            int i = 0;
-            if (nums[i] % 2 == 0);
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] % 2 == 0) ;
+            }
             return nums;
         }
     }
--- Nums/Runner.java
+++ Nums/Runner.java
@@ -1,8 +1,11 @@
 package task.Nums;
+
+import java.util.Arrays;
+
 public class Runner {
 
     public static void main(String[] args){
         int[] number = EvenNumbers.arrayEvenNums();
-        System.out.println(number);
+        System.out.println(Arrays.toString(number));
     }
 }
