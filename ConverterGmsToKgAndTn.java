fc36f28c2db273a8fc4960ab6ab149f7e65f3d8e
Banjeazy <45882335+Banjeazy@users.noreply.github.com>
2020-04-18 17:01:42 +0300

    Update ConverterGmsToKgAndTn.java

--- ConverterGmsToKgAndTn.java
+++ ConverterGmsToKgAndTn.java
@@ -1,17 +1,17 @@
 package Tasks1;
 
-public class ConverterGmsToKgAndTn {
+public class ConverterGrammsToKgAndTn {
     public static void main(String[] args){
-        ConverterGmsToKgAndTn con = new ConverterGmsToKgAndTn();
+        ConverterGrammsToKgAndTon con = new ConverterGrammsToKgAndTon();
         con.convertToKg(200);
         con.convertToTn(10000);
 
     }
-    public static double convertToKg(double gms){
-        return gms * 0.001;
+    public static double convertToKg(double gramms){
+        return gramms * 0.001;
     }
-    public static double convertToTn(double gms){
-        return gms * 0.000001;
+    public static double convertToTon(double gramms){
+        return gramms * 0.000001;
     }
 
 }
